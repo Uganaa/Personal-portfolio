0c367ef285ee49d2cce510f7d6967d47f1646055
Uganaa <uuganaa1113@gmail.com>
2024-06-17 22:39:09 -0400

    add eventlistener

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementsById('button').addEventListener('click', function(){
+    var element = document.getElementsByName('body');
+    element.style.backgroundColor = '#333';
+    element.style.color = '#f4f4f4';
+});
