05810cec7ba4521286b1871d7aaa909f85e57000
Uganaa <uuganaa1113@gmail.com>
2024-06-22 13:49:07 -0400

    delete unnecessary lines of code

--- main.js
+++ main.js
@@ -7,7 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
             element.style.backgroundColor = '#333';
             console.log("completed");
         } else {
-        console.log("No elements found with the name 'header'.");
+            console.log("No elements found with the name 'header'.");
         }
     })
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const myButton = document.getElementById('myButton');
+    myButton.addEventListener('click', () => {
+        var element = document.querySelector('section');
+            element.style.color = '#f4f4f4';
+            element.style.backgroundColor = '#333';
+            console.log("Completed");
+    });
 });
